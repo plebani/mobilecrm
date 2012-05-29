0266e776b90a00a670b045eb5fac26c49473b550
Alex Plebani <alexplebani@gmail.com>
2012-05-29 17:43:49 -0300

    segundo comit

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -13,6 +13,33 @@ namespace mobileCRM.Controllers
         
         public ActionResult Index()
         {
+            ViewBag.informacao = string.Empty;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            string email = string.Empty;
+            string password = string.Empty;
+
+            if (collection.Count > 0)
+            {
+                email = collection["email"];
+                password = collection["password"];
+
+                if ((email == "fernando.alex@benner.com.br") && (password == "123"))
+                {
+                    ViewBag.informacao = "Logando...";
+                    System.Threading.Thread.Sleep(1000);
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.informacao = "Babacaaaa";
+                }
+            }
+
             return View();
         }
     }
